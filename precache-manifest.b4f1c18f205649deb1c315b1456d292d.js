eb7aa31a6b72051bd87ff8950c5bd2a40e3aad75
RedZelios <dshonamhenry@gmail.com>
2019-05-07 12:50:02 -0400

    init commit

---
+++ precache-manifest.b4f1c18f205649deb1c315b1456d292d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1280e13c6f1c329a75acde88550f95f0",
+    "url": "/Journal/static/media/banner.1280e13c.png"
+  },
+  {
+    "revision": "3d72b80a3bf00bc6b529",
+    "url": "/Journal/static/css/main.96e279a9.chunk.css"
+  },
+  {
+    "revision": "be89a0ed90784764ba6a",
+    "url": "/Journal/static/js/1.be89a0ed.chunk.js"
+  },
+  {
+    "revision": "bf0af54e2e52b110cfa2",
+    "url": "/Journal/static/js/runtime~main.bf0af54e.js"
+  },
+  {
+    "revision": "218427837b8a6a7a8fa08496f133619f",
+    "url": "/Journal/static/media/face.21842783.png"
+  },
+  {
+    "revision": "6522f3b18198d50b08e3afdde4e6d3ce",
+    "url": "/Journal/static/media/candle.6522f3b1.png"
+  },
+  {
+    "revision": "3d72b80a3bf00bc6b529",
+    "url": "/Journal/static/js/main.3d72b80a.chunk.js"
+  },
+  {
+    "revision": "c89e9e5078df6a028d545000728657f2",
+    "url": "/Journal/static/media/cig.c89e9e50.gif"
+  },
+  {
+    "revision": "a9bfd1722e77a0dfb0357ed0add7f399",
+    "url": "/Journal/static/media/city.a9bfd172.gif"
+  },
+  {
+    "revision": "6f1b983e999ddbce23d73218c6088cb5",
+    "url": "/Journal/static/media/head.6f1b983e.png"
+  },
+  {
+    "revision": "f7b471e26cfae3b66385517a3206bedb",
+    "url": "/Journal/static/media/marker.f7b471e2.png"
+  },
+  {
+    "revision": "0ec17e7ed8f92515b9b8aef50281eb27",
+    "url": "/Journal/static/media/shoe.0ec17e7e.png"
+  },
+  {
+    "revision": "22e95b2ca91b16516a55dc3c76065cd1",
+    "url": "/Journal/index.html"
+  }
+];
